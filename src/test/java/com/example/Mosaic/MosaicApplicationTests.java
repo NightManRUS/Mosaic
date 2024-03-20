3f49f9426584278f69bf18cdbfd5e0352157efbc
Kirill <rA@>
2024-03-21 00:53:24 +0300

    first commit

---
+++ src/test/java/com/example/Mosaic/MosaicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mosaic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosaicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
