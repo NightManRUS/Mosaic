032bcf2cb4336547bb8bd3c36fc5bff8daef982e
Kirill <rA@>
2024-04-02 22:03:01 +0300

    add good fileChooser

--- src/main/java/com/example/Mosaic/mosaic/development/OperationButtonClickListener.java
+++ src/main/java/com/example/Mosaic/mosaic/development/OperationButtonClickListener.java
@@ -1,8 +1,10 @@
 package com.example.Mosaic.mosaic.development;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class OperationButtonClickListener implements ActionListener {
 
@@ -19,15 +21,18 @@ public class OperationButtonClickListener implements ActionListener {
         switch (buttonName) {
             //Если нажата кнопка Выбрать файл открывается проводник
             case "Выбрать файл":
-                JFileChooser fileChooser = new JFileChooser();
-                int returnValue = fileChooser.showOpenDialog(null);
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    JOptionPane.showMessageDialog(null, "Файл выбран: "
-                            + fileChooser.getSelectedFile().getName());
-                    inputFileName = fileChooser.getSelectedFile().getName();
+                FileDialog fileChooser = new FileDialog((java.awt.Frame) null, "Выберите файл", FileDialog.LOAD); // Создаем экземпляр FileDialog
+                fileChooser.setVisible(true); // Открываем диалоговое окно выбора файла
+
+                String selectedFilePath = fileChooser.getFile(); // Получаем выбранный файл
+                if (selectedFilePath != null) { // Проверяем, был ли выбран файл
+                    String selectedFileDirectory = fileChooser.getDirectory(); // Получаем директорию выбранного файла
+                    String selectedFileFullPath = selectedFileDirectory + selectedFilePath; // Полный путь к выбранному файлу
+                    inputFileName = selectedFileFullPath;
+                    System.out.println("Выбранный файл: " + selectedFileFullPath); // Выводим путь к выбранному файлу
                 } else {
-                    JOptionPane.showMessageDialog(null, "Файл не выбран");
-                };
+                    System.out.println("Файл не выбран"); // Если файл не выбран, выводим сообщение об этом
+                }
             case "Продолжить":
                 return;
             default:
